dc44a9f822a76ea5afb85f86ee5c2071ea830bf5
RJ Sheperd <rjsheperd@gmail.com>
2013-06-12 11:39:38 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,6 @@
+//index.js
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('hello, i know nodejitsu\n');
+}).listen(8080); 
